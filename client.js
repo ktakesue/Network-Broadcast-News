af3c7d068b363d3fa7d3af2e0ba2a35f49dd60a0
ktakesue <ktakesue@aol.com>
2018-02-24 16:01:31 -1000

    broadcasts between clients and admin

--- client.js
+++ client.js
@@ -1,5 +1,22 @@
 const net = require("net");
 
-const server = net.createConnection(6969, "0.0.0.0", () => {
-  server.write("HEWWWWWWWO");
+const client = net.connect(6969, "0.0.0.0", () => {
+  //   client.write("HEWWWWWWWO");
+  console.log("You're connected!");
+});
+
+process.stdin.on("data", data => {
+  client.write(data);
+});
+
+client.on("data", data => {
+  console.log(data.toString());
+});
+
+client.on("error", () => {
+  console.log("goodbye");
+});
+
+client.on("close", () => {
+  console.log("Connection closed");
 });
--- server.js
+++ server.js
@@ -1,30 +1,44 @@
 const net = require("net");
-// const PORT = 6969;
+const clients = [];
 
 const server = net.createServer(client => {
   console.log("Client connected!");
-  client.write("HELLO MOTHERTRUCKER");
-  client.on("data", data => {
-    console.log(data.toString());
+  client.write("HELLO MOTHERTRUCKER AND WELCOME");
+
+  client.setEncoding("utf8");
+  clients.push(client);
+  //   console.log("Client Names", clients);
+
+  process.stdin.on("readable", () => {
+    const chunk = process.stdin.read();
+    if (chunk !== null) {
+      clients.forEach(clientData => {
+        console.log(`ADMIN : ${chunk}`);
+        clientData.write(`ADMIN : ${chunk}`);
+      });
+    }
   });
+
+  client.on("data", data => {
+    clients.forEach(clientData => {
+      console.log(data.toString());
+      clientData.write(data.toString());
+    });
+  });
+
+  client.on("error", () => {
+    console.log("goodbye");
+  });
+
   client.on("end", () => {
-    client.write("BAI");
+    // client.write("SEE YA SPACE COWBOY");
+    console.log("SEE YA SPACE COWBOY");
   });
 });
+// process.stdin.on("end", () => {
+//   process.stdout.write("end");
+// });
 
 server.listen(6969, "0.0.0.0", () => {
   console.log(`Server started on port 6969`);
-});
-
-process.stdin.setEncoding("utf8");
-
-process.stdin.on("readable", () => {
-  const chunk = process.stdin.read();
-  if (chunk !== null) {
-    process.stdout.write(`data: ${chunk}`);
-  }
-});
-
-process.stdin.on("end", () => {
-  process.stdout.write("end");
 });
